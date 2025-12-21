0628996e3748cdd24fef0085d0b6a5b411ebbd06
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2025-12-21 19:04:11 +0200

    add home page images

--- src/pages/home/home-page.js
+++ src/pages/home/home-page.js
@@ -1,22 +1,20 @@
-import { initDropdown } from "@/shared/ui/dropdown/dropdown.js";
 import { initHeader } from "@/widgets/header/header.js";
-import { initProducts } from "./sections/products/products.js";
-import { initManufacturerSection } from "./sections/manufacturer/manufacturer.js";
-import { initNewsSection } from "@/widgets/news-section/news-section.js";
-import { initHero } from "./sections/hero/hero.js";
-import { initGoal } from "./sections/goal/goal.js";
+// import { initProducts } from "./sections/products/products.js";
+// import { initManufacturerSection } from "./sections/manufacturer/manufacturer.js";
+// import { initNewsSection } from "@/widgets/news-section/news-section.js";
+// import { initHero } from "./sections/hero/hero.js";
+// import { initGoal } from "./sections/goal/goal.js";
 import { lazyLoadElements } from "@/shared/helpers/lazy-loading/lazy-loading.js";
-import { initPageFooter } from "@/widgets/footer/footer.js";
+// import { initPageFooter } from "@/widgets/footer/footer.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-  initDropdown({ selector: ".top-header__lang" });
   initHeader();
-  initProducts();
-  initManufacturerSection();
-  initNewsSection();
-  initHero();
-  initGoal();
-  initPageFooter();
+  // initProducts();
+  // initManufacturerSection();
+  // initNewsSection();
+  // initHero();
+  // initGoal();
+  // initPageFooter();
 
   lazyLoadElements(".lazy", { rootMargin: "200px 0px" });
 });
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,27 +1,27 @@
-import { store } from "@/app/store/index.js";
-import { initAuthListener } from "@/entities/auth/model/auth-slice";
-import { fetchCartProducts } from "@/features/cart/model/cart-slice";
+// import { store } from "@/app/store/index.js";
+// import { initAuthListener } from "@/entities/auth/model/auth-slice";
+// import { fetchCartProducts } from "@/features/cart/model/cart-slice";
 
-async function initializeApp() {
-  initAuthListener();
-  initializeCart();
-}
+// async function initializeApp() {
+//   initAuthListener();
+//   initializeCart();
+// }
 
-initializeApp();
+// initializeApp();
 
-store.subscribe("auth", async (newState) => {});
+// store.subscribe("auth", async (newState) => {});
 
-store.subscribe("cart", async (newState) => {});
+// store.subscribe("cart", async (newState) => {});
 
-function initializeCart() {
-  try {
-    const cartItems = store.getState().cart.items;
+// function initializeCart() {
+//   try {
+//     const cartItems = store.getState().cart.items;
 
-    if (cartItems && cartItems.length > 0) {
-      fetchCartProducts();
-    } else {
-    }
-  } catch (error) {
-    console.error("Помилка під час ініціалізації кошика:", error);
-  }
-}
+//     if (cartItems && cartItems.length > 0) {
+//       fetchCartProducts();
+//     } else {
+//     }
+//   } catch (error) {
+//     console.error("Помилка під час ініціалізації кошика:", error);
+//   }
+// }
