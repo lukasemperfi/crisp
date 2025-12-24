add4da44ca7d430f93a3a35cbd2daa3f875868ac
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2025-12-24 19:04:57 +0200

    create product card markup and skeleton

--- src/pages/home/home-page.js
+++ src/pages/home/home-page.js
@@ -1,5 +1,5 @@
 import { initHeader } from "@/widgets/header/header.js";
-// import { initProducts } from "./sections/products/products.js";
+
 // import { initManufacturerSection } from "./sections/manufacturer/manufacturer.js";
 // import { initNewsSection } from "@/widgets/news-section/news-section.js";
 // import { initHero } from "./sections/hero/hero.js";
@@ -7,6 +7,7 @@ import { initHeader } from "@/widgets/header/header.js";
 import { lazyLoadElements } from "@/shared/helpers/lazy-loading/lazy-loading.js";
 import { initPageFooter } from "@/widgets/footer/footer";
 import { initHeroSection } from "./sections/hero-section/hero-section";
+import { initProducts } from "./sections/products-section/products-section";
 
 document.addEventListener("DOMContentLoaded", async () => {
   initHeader();
