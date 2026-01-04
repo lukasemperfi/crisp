f76e7d8c7f5e8028b1c1848c7240e4d44080c3cd
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2026-01-04 22:44:32 +0200

    init product page

---
+++ src/pages/product/product.js
@@ -0,0 +1,15 @@
+import { initHeader } from "@/widgets/header/header.js";
+import { initPageFooter } from "@/widgets/footer/footer.js";
+import { lazyLoadElements } from "@/shared/helpers/lazy-loading/lazy-loading.js";
+import { initProductContent } from "./sections/product-content/product-content.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const params = new URLSearchParams(window.location.search);
+  const id = params.get("id");
+  // const product = await fetchProductById(id);
+
+  initHeader();
+  initProductContent({});
+  initPageFooter();
+  lazyLoadElements(".lazy", { rootMargin: "200px 0px" });
+});
---
+++ src/pages/product/sections/product-content/product-content.js
@@ -0,0 +1,1 @@
+export const initProductContent = (product) => {};
