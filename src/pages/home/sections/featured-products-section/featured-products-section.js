42221f10f8e624f8cdc5dfe8fd799ed16d7fa930
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2026-01-10 15:33:12 +0200

    refactor featured section options

--- src/pages/home/sections/featured-products-section/featured-products-section.js
+++ src/pages/home/sections/featured-products-section/featured-products-section.js
@@ -2,9 +2,7 @@ import { productsApi } from "@/entities/product/api/products";
 import { initProductSlider } from "@/entities/product/ui/product-slider/product-slider";
 
 export const initFeaturedProductsSection = async () => {
-  initProductSlider({
-    containerSelector: ".featured-slider__slider-container",
-    fetchProducts: productsApi.getFeaturedProducts,
+  const commonOptions = {
     totalLimit: 16,
     pageSizeMobile: 4,
     swiperOptions: {
@@ -14,6 +12,12 @@ export const initFeaturedProductsSection = async () => {
         1920: { slidesPerView: 5, spaceBetween: 27 },
       },
     },
+  };
+
+  initProductSlider({
+    ...commonOptions,
+    containerSelector: ".featured-slider__slider-container",
+    fetchProducts: productsApi.getFeaturedProducts,
     navSelectors: {
       prevEl: ".featured-slider__left-nav",
       nextEl: ".featured-slider__right-nav",
@@ -21,17 +25,9 @@ export const initFeaturedProductsSection = async () => {
   });
 
   initProductSlider({
+    ...commonOptions,
     containerSelector: ".popular-slider__slider-container",
     fetchProducts: productsApi.getPopularProducts,
-    totalLimit: 16,
-    pageSizeMobile: 4,
-    swiperOptions: {
-      breakpoints: {
-        471: { slidesPerView: 3, spaceBetween: 16 },
-        1024: { slidesPerView: 4, spaceBetween: 20 },
-        1920: { slidesPerView: 5, spaceBetween: 27 },
-      },
-    },
     navSelectors: {
       prevEl: ".popular-slider__left-nav",
       nextEl: ".popular-slider__right-nav",
