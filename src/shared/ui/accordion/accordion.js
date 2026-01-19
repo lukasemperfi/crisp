7f0d99b3b38d338f432e69fd54cd70722b391341
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2026-01-20 00:52:47 +0200

    create accordion2

--- src/shared/ui/accordion/accordion.js
+++ src/shared/ui/accordion/accordion.js
@@ -1,3 +1,5 @@
+import { createComponent } from "../../lib/core/core";
+
 export const Accordion = (containerSelector, items, config = {}) => {
   const container = document.querySelector(containerSelector);
   if (!container) {
@@ -47,3 +49,52 @@ export const Accordion = (containerSelector, items, config = {}) => {
     });
   });
 };
+
+export function Accordion2(props) {
+  return createComponent(props, {
+    tag: "div",
+
+    render(el, props, emit, { runOnce }) {
+      const { items = [], isSingleOpen = true } = props;
+
+      if (runOnce) {
+        el.className = "accordion";
+
+        el.innerHTML = items
+          .map(
+            (item) => `
+              <div class="accordion__item ${item.isActive ? "is-active" : ""}">
+                <button class="accordion__button" type="button">
+                  <span class="accordion__title">${item.title}</span>
+                  <span class="accordion__icon"></span>
+                </button>
+
+                <div class="accordion__content">
+                  <div class="accordion__inner">
+                    ${item.content}
+                  </div>
+                </div>
+              </div>
+            `
+          )
+          .join("");
+
+        el._items = Array.from(el.querySelectorAll(".accordion__item"));
+
+        el._items.forEach((itemEl) => {
+          const btn = itemEl.querySelector(".accordion__button");
+
+          btn.addEventListener("click", () => {
+            const isActive = itemEl.classList.contains("is-active");
+
+            if (isSingleOpen && !isActive) {
+              el._items.forEach((el) => el.classList.remove("is-active"));
+            }
+
+            itemEl.classList.toggle("is-active");
+          });
+        });
+      }
+    },
+  });
+}
